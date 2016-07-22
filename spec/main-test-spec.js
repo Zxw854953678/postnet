dee0f97f07c45a6c951fb01d4724679e03aa884c
Zxw854953678 <Zxw854953678@163.com>
2016-07-22 20:13:31 +0800

    first commit correct

--- spec/main-test-spec.js
+++ spec/main-test-spec.js
@@ -1,7 +1,131 @@
-const app = require('../src/main');
+'use strict';
 
-describe('main', () => {
-  it('can work', () => {
-    expect(app.main()).toBe('Hello!');
+const test = require('../src/main');
+
+describe('buildChecksum funtion',()=>{
+
+  it('print checksum',()=>{
+    const postcode = '16749';
+    const barcodeInfo = test.buildChecksum(postcode);
+    const expectBarcodeInfo = {barsDigit:[1,6,7,4,9],cd:3};
+
+    expect(barcodeInfo).toEqual(expectBarcodeInfo);
   });
+
+  it('print checksum',()=>{
+    const postcode = '167490000';
+    const barcodeInfo = test.buildChecksum(postcode);
+    const expectBarcodeInfo = {barsDigit:[1,6,7,4,9,0,0,0,0],cd:3};
+
+    expect(barcodeInfo).toEqual(expectBarcodeInfo);
+  });
+
+  it('print checksum',()=>{
+    const postcode = '16749-0000';
+    const barcodeInfo = test.buildChecksum(postcode);
+    const expectBarcodeInfo = {barsDigit:[1,6,7,4,9,0,0,0,0],cd:3};
+
+    expect(barcodeInfo).toEqual(expectBarcodeInfo);
+  });
+});
+
+describe('buildBarcodesStr function',()=>{
+  const barcodeInfo = {barsDigit:[1,6,7,4,9],cd:3};
+
+  it('print barcodesStr',()=>{
+    const allBarsNum =test.loadAllBars();
+    const barsStr = test.buildBarsStr(barcodeInfo,allBarsNum);
+    const expectBarsStr = [':::||',':||::','|:::|',':|::|','|:|::','::||:'];
+
+    expect(barsStr).toEqual(expectBarsStr);
+  });
+});
+
+describe('buildBarcode funtion',()=>{
+  const barsStr = [':::||',':||::','|:::|',':|::|','|:|::','::||:'];
+
+  it('print barcode',()=>{
+    const bar = test.buildBarcode(barsStr);
+    const expectBar = '|:::||:||::|:::|:|::||:|::::||:|';
+
+    expect(bar).toEqual(expectBar);
+  });
+});
+
+describe('printBar funtion',()=>{
+  it('print bar',()=>{
+    const postcode = '16749';
+    const allBarsNum = test.loadAllBars();
+    spyOn(console, 'log');
+    test.printBar(postcode,allBarsNum);
+    const expectBar = '|:::||:||::|:::|:|::||:|::::||:|';
+
+    expect(console.log).toHaveBeenCalledWith(expectBar);
+  });
+
+  it('print bar',()=>{
+    const postcodeError = '5678900976';
+    const err =test.printBar(postcodeError);
+    expect(err).toBe('postcode illegal');
+  });
+});
+
+describe('splitBarcode function',()=>{
+  const barcode = '|:::||:||::|:::|:|::||:|::::||:|';
+
+  it('print barcodesNum',()=>{
+    const allBarsNum = test.loadAllBars();
+    const barcodesNum = test.splitBarcode(barcode,allBarsNum);
+    const expectBarcodesNum = ['1','6','7','4','9','3'];
+
+    expect(barcodesNum).toEqual(expectBarcodesNum);
+  })
+});
+
+describe('isBarValid function',()=>{
+  it('print barValid',()=>{
+    const barDigits = ['1','6','7','4','9','3'];
+    const result = test.isBarValid(barDigits);
+    expect(!result).toBeFalsy();
+  })
+});
+
+describe('joinBarDigits function',()=>{
+  const barDigits = ['1','6','7','4','9','3'];
+
+  it('print postcode',()=>{
+    const postcode = test.joinBarDigits(barDigits);
+    const expectPostcode = '167493';
+
+    expect(postcode).toEqual(expectPostcode);
+  })
+});
+
+describe('prinPostcode function',()=>{
+  it('print postcode',()=>{
+    const allBarsNum = test.loadAllBars();
+    const bar = '|:::||:||::|:::|:|::||:|::::||:|';
+    spyOn(console, 'log');
+    test.printPostcode(bar,allBarsNum);
+    const expectPostcode = '16749';
+    expect(console.log).toHaveBeenCalledWith(expectPostcode);
+  });
+
+  it('print postcode',()=>{
+    const allBarsNum = test.loadAllBars();
+    const bar = '|:|::|:|:|:||::::|:|::||:::::||::|:|::||::|::|||:::|';
+    spyOn(console, 'log');
+    test.printPostcode(bar,allBarsNum);
+    const expectPostcode = '45056-1234';
+    expect(console.log).toHaveBeenCalledWith(expectPostcode);
+  });
+
+  it('print postcode',()=>{
+    const barErr = '|:::||:||::|:::|:|::||:|::|';
+    //spyOn(console, 'log');
+    const err = test.printPostcode(barErr);
+
+    const expectErr = 'barcode illegal';
+    expect(err).toEqual(expectErr);
+  })
 });
--- src/main.js
+++ src/main.js
@@ -1,6 +1,144 @@
+'use strict';
 
-function main() {
-  return 'Hello!';
+function loadAllBars() {
+  return [
+    '||:::',
+    ':::||',
+    '::|:|',
+    '::||:',
+    ':|::|',
+    ':|:|:',
+    ':||::',
+    '|:::|',
+    '|::|:',
+    '|:|::'
+  ];
+}
+function buildChecksum(postcode) {
+  const barStr = postcode.replace('-', '');
+  const barsDigit = barStr.split('').map(barsStr=>Number(barsStr));
+  const sum = barsDigit.reduce((prv, next)=> {
+    return prv + next;
+  }, 0);
+  const cd = getCd(sum);
+
+  return {barsDigit, cd};
 }
 
-exports.main = main;
+function getCd(sum) {
+  const result = 10 - sum % 10;
+  return result === 10 ? 0 : result;
+}
+
+function buildBarsStr(barcodeInfo, allBarsNum) {
+  let barsStr = barcodeInfo.barsDigit.map(barcodeNum=> {
+    return allBarsNum[barcodeNum];
+  });
+
+  barsStr.push(allBarsNum[barcodeInfo.cd]);
+
+  return barsStr;
+}
+
+function buildBarcode(barsStr) {
+  let bar = barsStr.reduce((prv, next)=> {
+    return prv + next
+  });
+  return `|${bar}|`;
+}
+
+function printBar(postcode, allBarsNum) {
+  if (!judgeLegal(postcode)) {
+    return 'postcode illegal';
+  }
+  const barcodeInfo = buildChecksum(postcode);
+  const barsStr = buildBarsStr(barcodeInfo, allBarsNum);
+  const bar = buildBarcode(barsStr);
+  console.log(bar);
+}
+
+function judgeLegal(postcode) {
+  return (postcode.length === 5 || postcode.length === 9 || (postcode.length === 10 && (postcode.indexOf('-') >= 0)));
+}
+
+function splitBarcode(bar, allBarsNum) {
+  const barcode = bar.substring(1, bar.length - 1);
+  let barcodeInfo = [];
+
+  barcode.split('').forEach((bar, index)=> {
+    if (isMultiple(index)) {
+      const barStr = barcode.substr(index, 5);
+      const barDigit = allBarsNum.findIndex(allBarNum=>allBarNum === barStr);
+      barcodeInfo.push(barDigit.toString());
+    }
+  });
+
+  return barcodeInfo;
+}
+
+function isBarValid(barDigits) {
+  const sum = barDigits.reduce((prv, next)=> {
+    return parseInt(prv) + parseInt(next)
+  }, 0);
+
+  return (sum % 10 === 0);
+}
+
+function isMultiple(barcodeNum) {
+  return barcodeNum % 5 === 0;
+}
+
+function joinBarDigits(barDigits) {
+  return barDigits.reduce((prv, next)=> {
+    return prv + next;
+  });
+}
+
+function printPostcode(bar, allBarsNum) {
+  if (!checkBar(bar)) {
+    return 'barcode illegal';
+  }
+
+  const barDigits = splitBarcode(bar, allBarsNum);
+
+  if (!isBarValid(barDigits)) {
+    return 'verification fails';
+  }
+
+  let postcode = joinBarDigits(barDigits);
+  if (postcode.length === 6)
+    console.log(substringPostcode(postcode));
+  else {
+    console.log(substringPostcode(insertSymbol(postcode)));
+  }
+
+}
+
+
+function insertSymbol(postcode) {
+  const splitPostcode = postcode.split('');
+  splitPostcode.splice(splitPostcode.length - 5, 0, '-');
+  return splitPostcode.join('');
+}
+
+function substringPostcode(bar) {
+  return bar.substring(0, bar.length - 1);
+}
+
+function checkBar(bar) {
+  return (bar.length % 6 === 2 || bar.length % 10 === 2);
+}
+
+module.exports = {
+  buildChecksum: buildChecksum,
+  buildBarsStr: buildBarsStr,
+  buildBarcode: buildBarcode,
+  printBar: printBar,
+
+  splitBarcode: splitBarcode,
+  isBarValid: isBarValid,
+  joinBarDigits: joinBarDigits,
+  printPostcode: printPostcode,
+
+  loadAllBars: loadAllBars
+};
